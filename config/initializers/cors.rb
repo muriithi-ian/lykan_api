6d3d0771d404d0fd2291ecea43d9f5ef01a285bd
Muriithi Ian <33234188+muriithi-ian@users.noreply.github.com>
2023-01-20 11:28:29 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:4000', 'localhost'
+      origins 'localhost:4000', 'localhost, lykan-muriithi-ian.vercel.app'
   
       resource '*',
         headers: :any,
